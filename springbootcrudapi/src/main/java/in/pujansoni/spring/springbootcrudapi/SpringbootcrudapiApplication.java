ea9a5b5467f7b8ba358f6be3eddf393364948162
pujansoni <pujansoni1997@gmail.com>
2020-03-03 13:18:55 -0500

    Updating the main file springbootcrudapi application

--- springbootcrudapi/src/main/java/in/pujansoni/spring/springbootcrudapi/SpringbootcrudapiApplication.java
+++ springbootcrudapi/src/main/java/in/pujansoni/spring/springbootcrudapi/SpringbootcrudapiApplication.java
@@ -13,4 +13,5 @@ public class SpringbootcrudapiApplication {
 }
 // In the postman the GET request should be: localhost:8080/api/employee
 // In the postman the POST request select the body and select raw and select the type of JSON and insert data, url should be: localhost:8080/api/employee. In addition, the data should be passed in the JSON format without the id parameter as it is autoincrement
-// In the postman to GET a single employee use the get method and the URL: localhost:8080/api/employee/2. Here 2 is the id and can be any valid id. 
+// In the postman to GET a single employee use the get method and the URL: localhost:8080/api/employee/2. Here 2 is the id and can be any valid id.
+// In the postman to PUT(update) data we use the PUT method in post man and select body and raw and type to JSON and supply our update value at the url: localhost:8080/api/employee.
