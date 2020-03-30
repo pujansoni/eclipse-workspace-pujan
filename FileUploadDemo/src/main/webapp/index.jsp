5ac13c4baf416791eec9fc0d62210df0948c2ba2
pujansoni <pujansoni1997@gmail.com>
2020-03-30 16:13:48 -0400

    Adding LoginRegisterSession Project

---
+++ FileUploadDemo/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="upload" method="post">
+	<input type="file" name="file"/>
+	<input type="submit">
+</form>
+</body>
+</html>
---
+++ LoginRegisterSession/src/com/test/LoginDao.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class LoginDao {
+	String sql = "select * from login where uname=? and pass=?";
+	String url = "jdbc:mysql://localhost:3306/ems";
+	String username = "root";
+	String password = "root";
+	public boolean check(String uname, String pass) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, username, password);
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, uname);
+			st.setString(2, pass);
+			ResultSet rs = st.executeQuery();
+			if(rs.next()) {
+				return true;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
